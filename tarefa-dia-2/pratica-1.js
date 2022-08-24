df9196f985b13f7c1caee36f1dee5b62b2a97ad4
Katia-Leao <kastiaska@gmail.com>
2022-08-24 20:19:23 -0300

    aprendi a chamar a variável na string

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -8,3 +8,7 @@ console.log(nome + "/" + email + "/" + profissao + "/" + idade + "/" + lugar)
 
 //DESAFIO OPCIONAL
 console.log("Olá " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este e-mail, pois acabamos de confirmar as compras das passagens aéreas para " + lugar + ". Confirma pra gente alguns dados? A sua idade é " + idade + " anos, você é " + profissao + " e o seu e-mail é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D")
+
+//outra forma de escrever é: (aprendi na aula: coloca craze ao invés das aspas e chama a variável com cifrão e chaves)
+console.log(`Olá ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este e-mail, pois acabamos de confirmar as compras das passagens aéreas para ${lugar}. Confirma pra gente alguns dados? A sua idade é ${idade} anos, você é ${profissao} e o seu e-mail é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
+
