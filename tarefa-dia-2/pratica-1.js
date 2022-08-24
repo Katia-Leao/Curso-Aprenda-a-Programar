88108b15a36d365e3e3cf173b78a23db10d152ce
Katia-Leao <kastiaska@gmail.com>
2022-08-24 18:42:08 -0300

    primeiros codigos do ITuring, aula 2

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,10 @@
-var meunome = "Katia"
-var meuemail = "katiaska@gmail.com"
+var nome = "Katia"
+var email = "katiaska@gmail.com"
 var profissao = "futura programadora"
 var idade = 35
 var lugar = "Maldivas"
+
+console.log(nome + "/" + email + "/" + profissao + "/" + idade + "/" + lugar)
+
+//DESAFIO OPCIONAL
+console.log("Olá " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este e-mail, pois acabamos de confirmar as compras das passagens aéreas para " + lugar + ". Confirma pra gente alguns dados? A sua idade é " + idade + " anos, você é " + profissao + " e o seu e-mail é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D")
